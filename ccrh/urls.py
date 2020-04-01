79b4bbe1e6ef18fccfa3d87cdab59edbd1e12173
manish.kumar <manish.kumar@17700dac-6dd3-4d66-95e2-131c029fd464>
2020-04-01 00:10:46 +0000

    cms url added

--- ccrh/urls.py
+++ ccrh/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = i18n_patterns(
     url('admin/', admin.site.urls),
+    url('', include('cms.urls')),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
