b04f8fe9be82662bddb5c535959df362e0a5b90e
manish.kumar <manish.kumar@17700dac-6dd3-4d66-95e2-131c029fd464>
2020-04-13 00:09:29 +0000

    django admins for new media & downloads

--- master/admin.py
+++ master/admin.py
@@ -1,3 +1,40 @@
+"""
+Project    : "CCRH"
+module     : master/admin
+created    : 03/03/2020
+Author     : Manish Kumar
+"""
+
 from django.contrib import admin
+from master.models import (News,
+                           Downloads,
+                           QuickLink,
+                           Media,
+                           MediaMapping,
+                           )
 
 # Register your models here.
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('news_headline', 'desc')
+    search_fields = ('news_headline',)
+    
+class DownloadsAdmin(admin.ModelAdmin):
+    list_display = ('download_headline', 'desc')
+    search_fields = ('download_headline',)
+    
+class QuickLinkAdmin(admin.ModelAdmin):
+    list_display = ('headline', 'link')
+    search_fields = ('headline',)
+    
+class MediaAdmin(admin.ModelAdmin):
+    list_display = ('headline', )
+    search_fields = ('headline',)
+    
+class MediaMappingAdmin(admin.ModelAdmin):    
+    list_display = ('headline_name', )
+    
+admin.site.register(News, NewsAdmin)
+admin.site.register(Downloads, DownloadsAdmin)
+admin.site.register(QuickLink, QuickLinkAdmin)
+admin.site.register(Media, MediaAdmin)
+admin.site.register(MediaMapping, MediaMappingAdmin)
