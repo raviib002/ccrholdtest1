639d81547eef1aa2d0466540fb3b7738cad2bf2c
baryonsit <63849752+baryonsit@users.noreply.github.com>
2020-09-02 11:47:21 +0530

    Update settings.py

--- ccrh/settings.py
+++ ccrh/settings.py
@@ -117,9 +117,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', 
         'NAME': 'ccrh',
-        'USER': 'administrator1@ccrhmariadb',
-        'PASSWORD': 'Welcome@123',
-        'HOST': 'ccrhmariadb.mariadb.database.azure.com',   # Or an IP Address that your DB is hosted on
+        'USER': 'rooot@ccrhmariadbb',
+        'PASSWORD': 'Baryons@12345',
+        'HOST': 'ccrhmariadbb.mariadb.database.azure.com',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
