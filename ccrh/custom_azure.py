36b6799ec643fbe3018904d4c9b5daff94416a4e
baryonsit <63849752+baryonsit@users.noreply.github.com>
2020-09-03 00:00:15 +0530

    Create custom_azure.py

---
+++ ccrh/custom_azure.py
@@ -0,0 +1,13 @@
+from storages.backends.azure_storage import AzureStorage
+
+class AzureMediaStorage(AzureStorage):
+    account_name = 'ccrhstaticfiles' # Must be replaced by your <storage_account_name>
+    account_key = 'eOYVAq+sn0mHR3YG0RFlMXde5su5Hdfjo/kWziqPPBih7P3YjTsaQsoKe/KNZpvhzBfQ0dJGUlLXHmSWfJaSCA==' # Must be replaced by your <storage_account_key>
+    azure_container = 'ccrhmedia'
+    expiration_secs = None
+
+class AzureStaticStorage(AzureStorage):
+    account_name = 'ccrhstaticfiles' # Must be replaced by your storage_account_name
+    account_key = 'eOYVAq+sn0mHR3YG0RFlMXde5su5Hdfjo/kWziqPPBih7P3YjTsaQsoKe/KNZpvhzBfQ0dJGUlLXHmSWfJaSCA==' # Must be replaced by your <storage_account_key>
+    azure_container = 'ccrhstatic'
+    expiration_secs = None
