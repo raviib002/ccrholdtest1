f209ac50763a288b6899dfd20f8f38a8ad0d3cc1
baryonsit <63849752+baryonsit@users.noreply.github.com>
2020-09-02 14:14:22 +0530

    Update azure.py

--- ccrh/azure.py
+++ ccrh/azure.py
@@ -3,6 +3,6 @@ from .settings import *
 
 DEBUG = False
 STATICFILES_STORAGE = 'storages.backends.azure_storage.AzureStorage'
-AZURE_ACCOUNT_NAME = os.getenv('AZ_STORAGE_ACCOUNT_NAME')
+AZURE_ACCOUNT_NAME = os.getenv('blobforccrh')
 AZURE_CONTAINER = os.getenv('AZ_STORAGE_CONTAINER')
-AZURE_ACCOUNT_KEY = os.getenv('AZ_STORAGE_KEY')
+AZURE_ACCOUNT_KEY = os.getenv('+sYzKcLXQLvtefDkSAEONpc346QUUKJOyS/DU126ACzB+czxeZedGBSYxP1tC0QockYCzINydntJBLfPn9bvVQ==')
