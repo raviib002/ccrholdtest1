21c16dc40e45948a055904c68f6d818a9617afb7
baryonsit <63849752+baryonsit@users.noreply.github.com>
2020-09-03 14:12:22 +0530

    Delete azure.py

--- ccrh/azure.py
+++
@@ -1,8 +0,0 @@
-# flake8: noqa
-from .settings import *
-
-DEBUG = False
-STATICFILES_STORAGE = 'storages.backends.azure_storage.AzureStorage'
-AZURE_ACCOUNT_NAME = os.getenv('blobforccrh')
-AZURE_CONTAINER = os.getenv('AZ_STORAGE_CONTAINER')
-AZURE_ACCOUNT_KEY = os.getenv('+sYzKcLXQLvtefDkSAEONpc346QUUKJOyS/DU126ACzB+czxeZedGBSYxP1tC0QockYCzINydntJBLfPn9bvVQ==')
