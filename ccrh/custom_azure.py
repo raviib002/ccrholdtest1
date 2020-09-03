980e60d5912ad60db14a1381352c0501d4c5a8fa
baryonsit <63849752+baryonsit@users.noreply.github.com>
2020-09-03 22:14:42 +0530

    Update custom_azure.py

--- ccrh/custom_azure.py
+++ ccrh/custom_azure.py
@@ -2,13 +2,13 @@
 from storages.backends.azure_storage import AzureStorage
 
 class AzureMediaStorage(AzureStorage):
-    account_name = 'ccrhstaticfiles' # Must be replaced by your <storage_account_name>
-    account_key = 'eOYVAq+sn0mHR3YG0RFlMXde5su5Hdfjo/kWziqPPBih7P3YjTsaQsoKe/KNZpvhzBfQ0dJGUlLXHmSWfJaSCA==' # Must be replaced by your <storage_account_key>
+    account_name = 'ccrhhot' # Must be replaced by your <storage_account_name>
+    account_key = 'hSl5OWt2U0wuOwIKWCUW/2vJxA3Qm3P+SW5HPMCkYT3h46K5UluPzTqzLe5jaj0q61uUIzxNyPVzV80aPGibwg==' # Must be replaced by your <storage_account_key>
     azure_container = 'ccrhmedia'
     expiration_secs = None
 
 class AzureStaticStorage(AzureStorage):
-    account_name = 'ccrhstaticfiles' # Must be replaced by your storage_account_name
-    account_key = 'eOYVAq+sn0mHR3YG0RFlMXde5su5Hdfjo/kWziqPPBih7P3YjTsaQsoKe/KNZpvhzBfQ0dJGUlLXHmSWfJaSCA==' # Must be replaced by your <storage_account_key>
+    account_name = 'ccrhhot' # Must be replaced by your storage_account_name
+    account_key = 'hSl5OWt2U0wuOwIKWCUW/2vJxA3Qm3P+SW5HPMCkYT3h46K5UluPzTqzLe5jaj0q61uUIzxNyPVzV80aPGibwg==' # Must be replaced by your <storage_account_key>
     azure_container = 'ccrhstatic'
     expiration_secs = None
